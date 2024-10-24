efa9fa2727cea2f8afdd5f15739a8b24e546e02d
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-10-24 18:54:19 -0400

    premier composant

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
-
+  export default {
+    data() {
+      return {
+        nom: "John doe"
+      }
+    }
+  }
 </script>
 
 <template>
+  <h1>Bonjour {{ nom }}</h1>
 
 </template>
 
 <style>
+  h1 {
+    color: green;
+  }
 
 </style>
